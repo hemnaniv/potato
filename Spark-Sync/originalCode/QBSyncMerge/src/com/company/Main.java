2364e1a223775001109df027463d8d643aaac6ab
mkapadia <mkapadia@quickbase.com>
2019-08-27 13:16:43 -0400

    original simplified code

---
+++ Spark-Sync/originalCode/QBSyncMerge/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        Merge merger = new Merge();
+        merger.mergeRecords(source, destination);
+    }
+}
