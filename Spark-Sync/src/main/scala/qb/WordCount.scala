6ef5361b4945294843860f6c549ef320e68dde18
Hemnani <vhemnani@MB1904A.local>
2019-08-27 11:38:29 -0400

    updated proj to run

--- Spark-Sync/src/main/scala/qb/WordCount.scala
+++ Spark-Sync/src/main/scala/qb/WordCount.scala
@@ -1,16 +1,17 @@
-package wc
+package qb
+
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.log4j.LogManager
 import org.apache.log4j.Level
 
-object WordCountMain {
-  
+object SyncMain {
+
   def main(args: Array[String]) {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
     if (args.length != 2) {
-      logger.error("Usage:\nwc.WordCountMain <input dir> <output dir>")
+      logger.error("Usage:\nqb.SyncMain <input dir> <output dir>")
       System.exit(1)
     }
     val conf = new SparkConf().setAppName("Word Count")
@@ -21,7 +22,7 @@ object WordCountMain {
   //  val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
   //  try { hdfs.delete(new org.apache.hadoop.fs.Path(args(1)), true) } catch { case _: Throwable => {} }
 	// 	================
-    
+
     val textFile = sc.textFile(args(0))
     val counts = textFile.map(line => line.split(","))
                       .flatMap(word => List((word(0), 0), (word(1), 1))) //mapping unfollowed users too : bonus challenge 2
