f561d696fc8ffd38574cc33da755882459c7d261
Hemnani <vhemnani@MB1904A.local>
2019-08-27 11:54:05 -0400

    update src code

--- Spark-Sync/src/main/scala/qb/WordCount.scala
+++ Spark-Sync/src/main/scala/qb/WordCount.scala
@@ -18,11 +18,12 @@ object SyncMain {
     val sc = new SparkContext(conf)
 
 	//  Delete output directory, only to ease local development; will not work on AWS. ===========
-  //  val hadoopConf = new org.apache.hadoop.conf.Configuration
-  //  val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
-  //  try { hdfs.delete(new org.apache.hadoop.fs.Path(args(1)), true) } catch { case _: Throwable => {} }
+   val hadoopConf = new org.apache.hadoop.conf.Configuration
+   val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
+   try { hdfs.delete(new org.apache.hadoop.fs.Path(args(1)), true) } catch { case _: Throwable => {} }
 	// 	================
 
+  // main code here
     val textFile = sc.textFile(args(0))
     val counts = textFile.flatMap(line => line.split(" "))
                  .map(word => (word, 1))
