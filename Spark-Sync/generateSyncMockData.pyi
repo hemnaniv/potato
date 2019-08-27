3a25e93b81af6c4352d114fc57ed54bf3ef2e72b
Austin Colcord <acolcord@quickbase.com>
2019-08-27 13:43:49 -0400

    python stuff

--- Spark-Sync/generateSyncMockData.pyi
+++ Spark-Sync/generateSyncMockData.pyi
@@ -1,17 +1,32 @@
 import json
 import sys
 
-def getRecords(numRecords, numValues):
-    return [{recordId: [("RecordFieldValue%s" % x) for x in range(numValues)]} for recordId in range(0, numRecords)]
+def getRecords(numRecords, numValues, updated=False):
+    recordDict = {}
+    for recordId in range(numRecords):
+        recordDict[recordId] = ["RecordField" + str(x) + ("updated" if updated else "") for x in range(numValues)]
 
+    return recordDict
 
 # RUN THIS PROGRAM BY RUNNING THIS IN THE CONSOLE: python generateSyncMockData.pyi 10 12
 numRecords = int(sys.argv[1])
 numValuesPerRecord = int(sys.argv[2])
-x = json.dumps(getRecords(numRecords, numValuesPerRecord))
-print(x)
+originalRecords = json.dumps(getRecords(numRecords, numValuesPerRecord))
+updatedRecords = getRecords(numRecords, numValuesPerRecord, updated=True)
 
-file = open("testSparkSyncDataSet.json","w")
-file.write(x)
+def getRecordIDsToFilter(records):
+    return [key for key in records if key % 2 == 0]
+
+deleteKeys = getRecordIDsToFilter(updatedRecords)
+for key in deleteKeys: del updatedRecords[key]
+
+print("originalRecords", len(originalRecords))
+file = open("testSparkSyncDataSetOriginal.json","w")
+file.write(originalRecords)
+file.close()
+
+print("updatedRecords", len(updatedRecords))
+file = open("testSparkSyncDataSetUpdated.json","w")
+file.write(json.dumps(updatedRecords))
 file.close()
 
