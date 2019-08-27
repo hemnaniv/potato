f8ac8ac19dff5c85feec603b2df5bd89e71820b7
Austin Colcord <acolcord@quickbase.com>
2019-08-27 12:53:36 -0400

    command args

--- Spark-Sync/generateSyncMockData.pyi
+++ Spark-Sync/generateSyncMockData.pyi
@@ -1,13 +1,17 @@
 import json
+import sys
 
 def getRecords(numRecords, numValues):
     return [{recordId: [("RecordFieldValue%s" % x) for x in range(numValues)]} for recordId in range(0, numRecords)]
 
-x = json.dumps(getRecords(20, 20))
+
+# RUN THIS PROGRAM BY RUNNING THIS IN THE CONSOLE: python generateSyncMockData.pyi 10 12
+numRecords = int(sys.argv[1])
+numValuesPerRecord = int(sys.argv[2])
+x = json.dumps(getRecords(numRecords, numValuesPerRecord))
 print(x)
 
 file = open("testSparkSyncDataSet.json","w")
-
 file.write(x)
 file.close()
 
