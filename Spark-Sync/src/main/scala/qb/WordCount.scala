8e30b986f3ddd5dc557789885ef3dd0e668496bb
hemnaniv <vhemnani@MB1904A.local>
2019-08-28 11:47:11 -0400

    code updated

--- Spark-Sync/src/main/scala/qb/WordCount.scala
+++ Spark-Sync/src/main/scala/qb/WordCount.scala
@@ -6,7 +6,7 @@ import org.apache.spark.SparkContext
 import org.apache.log4j.LogManager
 import org.apache.log4j.Level
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 
 import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.sql.functions.broadcast
@@ -33,22 +33,44 @@ object SyncMain {
    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
    try { hdfs.delete(new org.apache.hadoop.fs.Path(args(1)), true) } catch { case _: Throwable => {} }
 	// 	================
-  val tsvWithHeaderOptions: Map[String, String] = Map(
-  ("delimiter", "\t"), // Uses "\t" delimiter instead of default ","
-  ("header", "true"))  // Writes a header record with column names
 
   // main code here
 
-  val source = spark.read.format("json").json("../testSparkSyncDataSetOriginal.json")
+  val source = spark.read.csv("../testSparkSyncDataSetOriginal.txt")
+  val dest = spark.read.csv("../testSparkSyncDataSetUpdated.txt")
 
+  val deleted = source.as("src").
+      join(dest.as("dst"), ($"src._c0" === $"dst._c0"), "right")
+      .select($"dst._c0")
+      .filter($"src._c0".isNull)
 
-    // val textFile = sc.textFile(args(0))
-    // val counts = textFile.flatMap(line => line.split(" "))
-    //              .map(word => (word, 1))
-    //              .reduceByKey(_ + _)
-    // source.saveAsTextFile(args(1))
-    //  counts.saveAsTextFile(args(1))
-    // logger.info(counts.toDebugString)
-    
+  val inserted = source.as("src")
+      .join(dest.as("dst"), $"src._c0" === $"dst._c0", "left_outer")
+      .select($"src.*").filter($"dst._c0".isNull)
+
+  val updated = dest.except(source)
+
+  val tsvWithHeaderOptions: Map[String, String] = Map(
+              ("delimiter", "\t"), // Uses "\t" delimiter instead of default ","
+              ("header", "true"))  // Writes a header record with column names
+
+    inserted
+      .write
+      .mode(SaveMode.Overwrite)
+      .options(tsvWithHeaderOptions)
+      .csv("output/inserted")
+
+    deleted
+      .write
+      .mode(SaveMode.Overwrite)
+      .options(tsvWithHeaderOptions)
+      .csv("output/deleted")
+
+    updated
+      .write
+      .mode(SaveMode.Overwrite)
+      .options(tsvWithHeaderOptions)
+      .csv("output/updated")
+
   }
 }
